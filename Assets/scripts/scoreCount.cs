4fbeeaa49e25d2246ffb942a55835108f5f09feb
Thomastert <thomas@thoemda.com>
2015-10-06 12:32:04 +0200

    Score-counter en level placeholder
    :100: :alien:  :koala:

---
+++ Assets/scripts/scoreCount.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoreCount : MonoBehaviour {
+    int score = 0;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "ball")
+        {
+            score = score + 10;
+            Debug.Log(score);
+        }
+    }
+}
