944c26376a2f1515be5f0522251fadd14d4465ff
Jeffreeyy <17844@ma-web.nl>
2015-10-06 11:48:58 +0200

    sc vxcbvn,mbcxdghzcfgchjkvhcnzcdxscdfgchjkchxgvbfgzdhtsrdyjcgfxncfbzgdhfjguyjkvchxdzhtfyjgujkhcxdzchtrjykcghjf

---
+++ Assets/FlipperController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperController : MonoBehaviour {
+
+	public GameObject leftFlipper;
+	public GameObject rightFlipper;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LeftFlipper ();
+	}
+
+	void LeftFlipper()
+	{
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			//
+		}
+		
+		if(Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			//
+		}
+	}
+}
