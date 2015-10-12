dd37b216347d9118ed0db82a35fa7e74e7049798
Jeffreeyy <17844@ma-web.nl>
2015-10-12 10:27:34 +0200

    Added Flippers and Spring
    Spring doesn't work properly

---
+++ Assets/scripts/Flipper.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper : MonoBehaviour {
+
+	public bool Left;
+	public bool Right;
+
+	public Vector3 l_com;
+	public Vector3 r_com;
+
+	private float _forceAmount = 10f;
+	private Rigidbody rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody> ();
+		if(this.Left){
+			rb.centerOfMass = l_com;
+			
+		}
+		if(this.Right){
+			rb.centerOfMass = r_com;
+		}
+	}
+
+	void Update () {
+		Flippers ();
+	}
+
+	private void Flippers(){
+		if (this.Left)
+		{
+			if(Input.GetKey (KeyCode.A))
+			{
+				rb.AddForce(transform.forward * _forceAmount, ForceMode.Impulse);
+			}
+			else
+			{
+				rb.AddForce(-transform.forward * 2f, ForceMode.Impulse);
+			}
+		}
+
+
+		if (this.Right)
+		{
+			if(Input.GetKey (KeyCode.D))
+			{
+				rb.AddForce(transform.forward * _forceAmount, ForceMode.Impulse);
+			}
+			else
+			{
+				rb.AddForce(-transform.forward * 2f, ForceMode.Impulse);
+			}
+		}
+	}
+}
---
+++ Assets/scripts/Spring.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+
+	private bool _Reset = false;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckSpringKey ();
+	}
+
+	private void CheckSpringKey()
+	{
+		if(Input.GetKeyDown (KeyCode.Space))
+		{
+			UseSpring ();
+			_Reset = true;
+		}
+		if(Input.GetKeyUp (KeyCode.Space) && _Reset)
+		{
+			ResetSpring();
+			_Reset = false;
+			
+		}
+	}
+
+	private void UseSpring()
+	{
+		rb.AddForce(transform.forward * 75f, ForceMode.Impulse);
+	}
+
+	private void ResetSpring()
+	{
+		if(_Reset)
+		{
+			rb.AddForce(-transform.forward * 75f, ForceMode.Impulse);
+			_Reset = false;
+		}
+	}
+}
