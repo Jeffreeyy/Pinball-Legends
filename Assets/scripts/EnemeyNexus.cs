679b21a25069b975347da5d9eb2fb1eceb690cca
Thomastert <thomas@thoemda.com>
2015-10-13 12:17:47 +0200

    enemy nexus explodium
    :bomb:

---
+++ Assets/scripts/EnemeyNexus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemeyNexus : MonoBehaviour {
+
+    void Explode()
+    {
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Ball")
+        {
+            Explode();
+            transform.GetComponent<Renderer>().material.color = Color.grey;
+        }
+    }
+
+}
