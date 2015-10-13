f9a51d8ffe4a34babd6311ce7cc018d42e9c9dfd
Thomastert <thomas@thoemda.com>
2015-10-13 08:47:31 +0200

    Tweek ball fysics

--- Assets/scripts/Flipper.cs
+++ Assets/scripts/Flipper.cs
@@ -9,7 +9,7 @@ public class Flipper : MonoBehaviour {
 	public Vector3 l_com;
 	public Vector3 r_com;
 
-	private float _forceAmount = 10f;
+	private float _forceAmount = 15f;
 	private Rigidbody rb;
 
 	void Awake(){
