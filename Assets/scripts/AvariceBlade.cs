6ccc1d0e8847bd1333aad5e96bebfaac2f83e217
Lorenzo <18927@ma-web.nl>
2015-10-13 11:23:57 +0200

    change item material
    Freelo bois :zzz:

--- Assets/scripts/AvariceBlade.cs
+++ Assets/scripts/AvariceBlade.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuyItems : MonoBehaviour {
+public class AvariceBlade : MonoBehaviour {
 
+	public Material avariceBladeUnavailable;
+	public Renderer rend;
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void OnMouseDown(){
 		if (scoreCount.score >= 1200) {
+			rend.material = avariceBladeUnavailable;
 			scoreCount.score = scoreCount.score - 1200;
 			scoreCount.AvariceBlade = true;
 		}
