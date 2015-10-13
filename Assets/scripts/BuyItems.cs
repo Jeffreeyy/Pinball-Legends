c26e90dfa1c4884a1f828ecd811d7a24bf23b564
Lorenzo <18927@ma-web.nl>
2015-10-13 11:05:25 +0200

    Shader Change
    Freelo :feelsgood:

---
+++ Assets/scripts/BuyItems.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyItems : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		if (scoreCount.score >= 1200) {
+			scoreCount.score = scoreCount.score - 1200;
+			scoreCount.AvariceBlade = true;
+		}
+	}
+}
--- Assets/scripts/scoreCount.cs
+++ Assets/scripts/scoreCount.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class scoreCount : MonoBehaviour {
     public static int score = 0;
+	public static bool AvariceBlade = false;
+	private int BonusGold = 0;
+
+	void Update(){
+		if (AvariceBlade == true) {
+			BonusGold = 20;
+		}
+	}
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "Ball")
+        if (col.gameObject.tag == "Buff")
         {
-            score = score + 10;
-            Debug.Log(score);
+            score = score + 60 + BonusGold;
         }
+
+		if (col.gameObject.tag == "Turret")
+		{
+			score = score + 125 + BonusGold;
+		}
     }
 }
