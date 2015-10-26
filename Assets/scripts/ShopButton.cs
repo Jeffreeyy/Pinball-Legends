92cc0e5fbd81fd19bfdf548cc88d94c5c7d4ba57
Lorenzo <18927@ma-web.nl>
2015-10-26 11:01:16 +0100

    Added Guardian Angel Item
    :3 new item bois

--- Assets/scripts/ShopButton.cs
+++ Assets/scripts/ShopButton.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ShopButton : MonoBehaviour {
 
 	public GameObject panel;
+	private bool isShopOpen = false;
 	// Use this for initialization
 	void Start () {
 		OpenShop (panel);
@@ -11,10 +12,16 @@ public class ShopButton : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (panel) {
+			Time.timeScale = 0;
+			isShopOpen = true;
+		}
 	}
 
 	public void OpenShop(GameObject panel){
 		panel.SetActive (!panel.activeSelf);
+		if (isShopOpen == true) {
+			Time.timeScale = 1;
+		}
 	}
 }
