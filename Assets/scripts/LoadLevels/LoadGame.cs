c3859ff03a5752e05f481b123be1a12e4f9e2dd2
Lorenzo <18927@ma-web.nl>
2015-10-27 12:39:14 +0100

    fixed shop positioning
    :8ball:  8ball approves

---
+++ Assets/scripts/LoadLevels/LoadGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution (1600, 729, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnMouseDown(){
+		Application.LoadLevel ("Bronzo");
+	}
+}
---
+++ Assets/scripts/SetResolution.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution (1600, 729, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
