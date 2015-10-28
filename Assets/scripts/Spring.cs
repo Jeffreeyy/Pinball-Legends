4796f13677e580456c2654a8fe0bf636dfdd1fca
Jeffreeyy <17844@ma-web.nl>
2015-10-28 16:10:15 +0100

    fixed spring
    more fixes soon

--- Assets/scripts/Spring.cs
+++ Assets/scripts/Spring.cs
@@ -2,48 +2,69 @@ using UnityEngine;
 using System.Collections;
 
 public class Spring : MonoBehaviour {
-
-	private bool _Reset = false;
+	
 	private Rigidbody rb;
+	private bool _isFrozen = true;
+	private Vector3 _startPos;
+	private bool _isInPlace = true;
 	public AudioSource kickSound;
+	
 	// Use this for initialization
 	void Awake () {
 		rb = GetComponent<Rigidbody> ();
 		kickSound = GetComponent<AudioSource> ();
+		_startPos = transform.position;
+	}
+	
+	void Start()
+	{
+		FreezeStatus ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		CheckSpringKey ();
-	}
-
-	private void CheckSpringKey()
-	{
-		if(Input.GetKeyDown (KeyCode.Space))
+		PullSpring ();
+		if (!_isInPlace) 
 		{
-			UseSpring ();
-			_Reset = true;
+			if(transform.position.z >= _startPos.z)
+			{
+				rb.velocity = new Vector3(0, 0, 0);
+				_isInPlace = true;
+				_isFrozen = true;
+				FreezeStatus ();
+				transform.position = _startPos;
+			}
 		}
-		if(Input.GetKeyUp (KeyCode.Space) && _Reset)
+		
+	}
+	
+	private void PullSpring()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
 		{
-			ResetSpring();
-			_Reset = false;
-			
+			_isFrozen = false;
+			FreezeStatus ();		
+		} 
+		
+		else if (Input.GetKeyUp (KeyCode.Space)) {
+			kickSound.Play ();
+			_isFrozen = false;
+			rb.AddForce (transform.forward * 50f, ForceMode.Impulse);
+			_isInPlace = false;
+			FreezeStatus();
 		}
+		
 	}
-
-	private void UseSpring()
+	
+	private void FreezeStatus()
 	{
-		rb.AddForce(transform.forward * 75f, ForceMode.Impulse);
-		kickSound.Play ();
-	}
-
-	private void ResetSpring()
-	{
-		if(_Reset)
+		if(_isFrozen)
 		{
-			rb.AddForce(-transform.forward * 75f, ForceMode.Impulse);
-			_Reset = false;
+			rb.constraints |= RigidbodyConstraints.FreezePositionZ;
+		}
+		if(!_isFrozen)
+		{
+			rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
 		}
 	}
 }
