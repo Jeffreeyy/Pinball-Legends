50cf5da8c73f9042e0ae9a0fccffebc99e853a45
Thomastert <thomas@thoemda.com>
2015-10-30 12:00:30 +0100

    Game over scene
    :deciduous_tree:

--- Assets/scripts/Death.cs
+++ Assets/scripts/Death.cs
@@ -4,24 +4,29 @@ using System.Collections;
 public class Death : MonoBehaviour {
 	
 	private Vector3 _spawnPoint;
+    public int Lifes = 3;
 
 	void Awake()
 	{
 		_spawnPoint = new Vector3 (24.05f, 0.25f, 0.7f);
 	}
 
-    void Explode()
+    void Update() 
     {
-        //var exp = GetComponent<ParticleSystem>();
-        //exp.Play();        
+        if (Lifes == 0) 
+        {
+            Application.LoadLevel("GameOver");
+            Debug.Log(Lifes);
+        }
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "Ball")
+        if (col.gameObject.name == "Ball" && Lifes != 0)
         {
             //Explode();
 			col.gameObject.transform.position = _spawnPoint;
+            Lifes--;
             //transform.GetComponent<Renderer>().material.color = Color.grey;
         }
     }
